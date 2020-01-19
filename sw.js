8c784349f401e23ab7617d353ce3907efeb4f173
Pandurangan D <pandurangan.d@mrcooper.com>
2020-01-20 00:07:30 +0530

    example

--- sw.js
+++ sw.js
@@ -1,58 +1,41 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
-]
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('serviceworker-cache-polyfill.js');
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+        'style.css',
+        'logging.js'
+      ]);
     })
-  )
-})
+  );
+});
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
     })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+  );
+});
